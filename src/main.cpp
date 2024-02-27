e4fe6e20907cb44436a0415129b2f2c5fcbee248
Braxton Salyer <braxtonsalyer@gmail.com>
2024-02-26 22:34:43 -0600

    Update main to be a more complete example of functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,20 @@ using namespace hyperion::mpl; // NOLINT(google-build-using-namespace)
 
 [[nodiscard]] auto
 main([[maybe_unused]] i32 argc, [[maybe_unused]] const char* const* argv) -> i32 {
-    return static_cast<i32>(0_value);
+    constexpr auto add_const = [](MetaType auto type) noexcept {
+        return type.as_const();
+    };
+
+    return List<i32, usize, f32>{}
+        .zip(List<f64, i64, u32>{})
+        .apply(add_const)
+        .all_of(is_const)
+        .apply([](MetaValue auto value) {
+            if constexpr(decltype(value)::value) {
+                return Value<0, i32>{};
+            }
+            else {
+                return Value<-1, i32>{};
+            }
+        });
 }
